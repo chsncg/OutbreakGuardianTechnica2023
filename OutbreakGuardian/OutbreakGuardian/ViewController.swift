9c0db4ddd1b1c6d373346a423355f93163cfa5cd
chsncg <74510051+chsncg@users.noreply.github.com>
2023-10-22 11:05:03 -0400

    Add files via upload

---
+++ OutbreakGuardian/OutbreakGuardian/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OutbreakGuardian
+//
+//  Created by Stuti Chaurasia on 10/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
+
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
